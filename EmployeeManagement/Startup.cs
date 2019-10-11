6668a21774075acba8e6f3cddff0e460df04705f
Lokesh Kashyap <lokeshkashyap858@gmail.com>
2019-10-11 11:50:26 +0530

    Basics Of .Net Core

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -5,12 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -18,16 +26,59 @@ namespace EmployeeManagement
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            //--------------------------------------------------using Logger---------------------------Part 11-------------------
+            //--------------------------------------------------Using Middleware Pipeline--------------Part 11-------------------
+            //app.Use(async (context, next) =>
+            //{
+            //    logger.LogInformation("MW1: Incoming request");
+            //    await next();
+            //    logger.LogInformation("MW1: Outgoing response");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    logger.LogInformation("MW2: Incoming request");
+            //    await next();
+            //    logger.LogInformation("MW2: Outgoing response");
+            //});
+
+
+
+            //---------------------------------------Using Static Files--------------------------------Part 12-------------------------
+
+            //DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            //defaultFilesOptions.DefaultFileNames.Clear();
+            //defaultFilesOptions.DefaultFileNames.Add("foo.html");
+
+            //app.UseDefaultFiles(defaultFilesOptions);
+            //app.UseStaticFiles();
+
+            //--------------------------------------Using File Server Options---------------------------Part 12------------------------
+            //FileServerOptions fileServerOptions = new FileServerOptions();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
+            //app.UseFileServer(fileServerOptions);
+
+
+
+            app.UseSpaStaticFiles();
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+
+                //----------------------------------For throwing Exception in ---------------------Part 13---------------------------------
+
+                //throw new Exception("Some Error processing the Request");
+
+                //logger.LogInformation("MW3: Incoming request");
+                await context.Response.WriteAsync("Hello World");
+                //logger.LogInformation("MW3: Outgoing response");
             });
         }
     }
